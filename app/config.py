a01302d724755d1f4c4142af67b08038a2a53796
Nicola Moretto <nicolamoretto88@gmail.com>
2020-06-10 22:41:31 +0200

    Add configuration file with environment variables

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_TABLE = os.getenv("DB_TABLE")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_FOLDER_NAME = os.getenv("AWS_FOLDER_NAME")
+
